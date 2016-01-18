6bc3019695f696c220e0a4d9266aba82a89c5ca3
Kyriakos Ziakoulis <cooxlee@gmail.com>
2016-01-18 18:20:49 +0200

    fix: syntax in login_required decorator

--- pytalog/views.py
+++ pytalog/views.py
@@ -40,7 +40,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'username' not in login_session:
-            return redirect(url_for('login', next=request.url))
+            return redirect(url_for('showLogin', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
 
