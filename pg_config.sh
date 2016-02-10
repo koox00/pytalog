53f8a46329899e3c3e6160a726598c28f13c2970
Kyriakos Ziakoulis <cooxlee@gmail.com>
2016-02-10 23:52:14 +0200

    change: doesn't seem to work
    Should make the virtual env and activate it manually

--- pg_config.sh
+++ pg_config.sh
@@ -2,7 +2,8 @@ apt-get -qqy update
 apt-get -qqy install libpq-devel python-dev
 apt-get -qqy install python-sqlalchemy
 apt-get -qqy install python-pip
-pip install virtualenv &&
-cd /vagrant/pytalog &&
-virtualenv env &&
-. env/bin/activate
+pip install virtualenv 
+#&&
+#cd /vagrant/pytalog &&
+#virtualenv env &&
+#. env/bin/activate
